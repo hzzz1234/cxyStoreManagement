c0f45e1c600f80e3708160e0fb2851dfd65b23b5
huaz <pandiansinian@163.com>
2015-10-29 02:18:39 +0800

    jetty插件

---
+++ apptools/storemanager-parent/storemanager-web/src/main/java/com/cxy/apptools/App.java
@@ -0,0 +1,10 @@
+package com.cxy.apptools;
+
+/**
+ * Created by Administrator on 2015/10/29.
+ */
+public class App {
+    public static void main(String args[]){
+        System.out.println("xxxx");
+    }
+}
