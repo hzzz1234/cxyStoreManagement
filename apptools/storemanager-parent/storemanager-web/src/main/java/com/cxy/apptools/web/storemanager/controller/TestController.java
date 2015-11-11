980d7935fa5a4c348a35b628b97d916b656659c2
huaz <pandiansinian@163.com>
2015-11-12 03:59:59 +0800

    web配置

---
+++ apptools/storemanager-parent/storemanager-web/src/main/java/com/cxy/apptools/web/storemanager/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.cxy.apptools.web.storemanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+/**
+ * Created by Administrator on 2015/11/12.
+ */
+
+@Controller             //此注解代表是控制器，不需要在xml中配置
+@RequestMapping("/first")       //url
+public class TestController {
+
+    @RequestMapping("hello")             //方法url
+     public ModelAndView view() {
+        ModelAndView mv = new ModelAndView();  //设置返回页面逻辑名,不带页面名后缀
+        mv.addObject("message", "Hello World!");  //保存值到作用域中，key为msg
+        mv.setViewName("hello");
+        return mv;
+    }
+
+    @ResponseBody
+    @RequestMapping("hellojson")             //方法url
+    public Object json() {
+        ModelAndView mv = new ModelAndView();  //设置返回页面逻辑名,不带页面名后缀
+        mv.addObject("message", "Hello World!");  //保存值到作用域中，key为msg
+        mv.setViewName("hello");
+        return mv;
+    }
+}
