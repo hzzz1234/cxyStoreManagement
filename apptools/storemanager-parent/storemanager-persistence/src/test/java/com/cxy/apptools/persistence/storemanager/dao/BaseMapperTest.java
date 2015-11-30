e92332c1c5b00b274f2afc9b4f5e5e515ea2e507
huaz <pandiansinian@163.com>
2015-11-30 23:18:53 +0800

    a

--- apptools/storemanager-parent/storemanager-persistence/src/test/java/com/cxy/apptools/persistence/storemanager/dao/BaseMapperTest.java
+++ apptools/storemanager-parent/storemanager-persistence/src/test/java/com/cxy/apptools/persistence/storemanager/dao/BaseMapperTest.java
@@ -1,9 +1,13 @@
 package com.cxy.apptools.persistence.storemanager.dao;
 
+import com.cxy.apptools.domain.storemanager.Cxycurrency;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.InputStream;
 
@@ -14,25 +18,27 @@ public class BaseMapperTest {
 
     protected SqlSession session = null;
 
+    @Before
     public void init() throws Exception {
         InputStream is = Resources.getResourceAsStream("mybatis_config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
         session = sqlSessionFactory.openSession();
     }
 
+    @After
     public void close() throws Exception {
         if(null != session){
             session.close();
         }
     }
 
-//    @Test
-//    public void testSelectByPrimaryKey() throws Exception {
-//        CxycurrencyMapper cxycurrencyMapper = session.getMapper(CxycurrencyMapper.class);
-//        List<Cxycurrency> list = cxycurrencyMapper.selectAllValues();
-//        for(Cxycurrency c : list){
-//            System.out.println(c.toString());
-//        }
-//    }
+
+
+    @Test
+    public void testSelectByPrimaryKey() throws Exception {
+        CxycurrencyMapper cxycurrencyMapper = session.getMapper(CxycurrencyMapper.class);
+        Cxycurrency cu = cxycurrencyMapper.getCurrencyZh(1);
+        System.out.println(cu.getCurrencyid()+" "+cu.getCurrencyZh()+" "+cu.getCurrencyEn());
+    }
 
 }
